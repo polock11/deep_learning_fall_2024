87bb2221c5c153aa5ee1aa128f6239b0905ada29
Shakib Ibna Shameem Polock <50078050+polock11@users.noreply.github.com>
2024-12-21 05:14:19 +0200

    added structures dirs

---
+++ final_project/a/obervations.py
@@ -0,0 +1,12 @@
+# from a folder
+# default result on eval set is .79
+# added an extra 512 unit layer to the model and the result is .82
+# added gaus and clahe augmentation and the result is .8394 
+# added gaus, clahe and added a 512 unit layer to the model and the result is .8367 (current best on test set) (file name: 03_img_aug_extra_layer.ipynb)
+
+# moving forward with 03_img_aug.ipynb files config.
+
+
+
+# from b folder
+# current best on test set is .8308 (file name: 03_train_with_DeepDRiD.ipynb) deepDRIiD dataset trained on APTOS model
